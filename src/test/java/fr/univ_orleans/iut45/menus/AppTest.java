ac4ac5e8f452d5fca4a68378b6f38f528e1ee6f3
Joaquin "Joe" Lapin <Jo.Lapin@univ-hochmoehrefelder.fr>
2024-03-26 19:37:03 +0100

    Ajout d'un test pour menu.estComplet

--- src/test/java/fr/univ_orleans/iut45/menus/AppTest.java
+++ src/test/java/fr/univ_orleans/iut45/menus/AppTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 
-public class AppTest 
+public class AppTest
 {
     @Test
     public void testPrixPlats()
@@ -29,6 +29,35 @@ public class AppTest
         les_entrees.add(carottes_rapees);
         Menu ce_midi = new Menu(les_entrees, new ArrayList<Plat>(), new ArrayList<Plat>());
         assertEquals(1, ce_midi.getEntrees().size());
+    }
+
+    @Test
+    public void testMenuComplet()
+    {
+        Plat carottes_rapees = new Plat("Carottes Rapées", false, new ArrayList<String>(), 0.5);
+        ArrayList<Plat> les_entrees = new ArrayList<Plat>();
+        les_entrees.add(carottes_rapees);
+        Menu ce_midi = new Menu(les_entrees, new ArrayList<Plat>(), new ArrayList<Plat>());
+        assertEquals(false, ce_midi.estComplet());
+
+        ArrayList<String> allergenes_pb = new ArrayList<String>();
+        allergenes_pb.add("gluten");
+        allergenes_pb.add("fruits à coque");
+        Plat paris_brest = new Plat ("Paris-Brest", false, allergenes_pb, 2.5);
+        ArrayList<Plat> les_desserts = new ArrayList<Plat>();
+        les_desserts.add(paris_brest);
+
+        ArrayList<String> allergenes_lasagnes = new ArrayList<String>();
+        ArrayList<String> allergenes_tortilla = new ArrayList<String>();
+        allergenes_lasagnes.add("gluten");
+        allergenes_tortilla.add("oeufs");
+        Plat lasagnes = new Plat("Lasagnes", true, allergenes_lasagnes, 2.8);
+        Plat tortilla = new Plat("Tortilla", false, allergenes_tortilla, 1.8);
+        ArrayList<Plat> les_plats_principaux = new ArrayList<Plat>();
+        les_plats_principaux.add(lasagnes);
+        les_plats_principaux.add(tortilla);
+        Menu ce_soir = new Menu(les_entrees, les_plats_principaux, les_desserts);
+        assertTrue(ce_soir.estComplet());
     }
 
 }
